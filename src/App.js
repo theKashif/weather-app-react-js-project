731d7738e8e841d9b44e676b1e32254f6ec6ca46
Kashif Shehzad Baloch <87254179+beKashif@users.noreply.github.com>
2023-01-27 20:45:29 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -95,7 +95,7 @@ function App() {
         weather.loading && (
           <>
           <br></br>
-          <br></br>
+//           <br></br>
           <h4>Loading...</h4>
           </>
         )
